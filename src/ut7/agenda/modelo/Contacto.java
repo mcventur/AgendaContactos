8424fa4d52d930f5f9db448a8ae7526191440edc
mcventur <marcelinopenide@gmail.com>
2022-04-08 09:37:08 +0200

    Clases Contacto y Profesional

--- src/ut7/agenda/modelo/Contacto.java
+++ src/ut7/agenda/modelo/Contacto.java
@@ -1,6 +1,8 @@
 package ut7.agenda.modelo;
 
-public class Contacto {
+import java.util.Objects;
+
+public abstract class Contacto implements Comparable<Contacto>{
 	private String nombre;
 	private String apellidos;
 	private String telefono;
@@ -46,10 +48,70 @@ public class Contacto {
 		this.email = email;
 	}
 
+	/**
+	 * De un contacto se quiere saber cuál es la primera letra de su apellido
+	 * @return un char con la primera letra del apellido
+	 */
+	public char getPrimeraLetra(){
+		return apellidos.charAt(0);
+	}
+
 	@Override
 	public int hashCode() {
 		return email.hashCode();
-
 	}
 
+	/**
+	 * Dos contactos son iguales si además de pertenecer a la misma clase tienen el mismo apellido, nombre y
+	 * email.
+	 */
+	@Override
+	public boolean equals(Object o) {
+		//Comprobaciones estándar
+		if(o==null) return false; //o es null => no iguales
+		if(this==o) return true; // o y this son el mismo objeto => iguales (idénticos)
+		if(this.getClass()!=o.getClass()) return false; //Son de tipos distintos => NO iguales
+
+		//Comprobaciones adicionales
+		Contacto contacto = (Contacto) o;
+		return this.nombre.equals(contacto.nombre) &&
+				this.apellidos.equals(contacto.getApellidos()) &&
+				this.email.equals(contacto.getEmail());
+	}
+
+	/**
+	 * Dos contactos se pueden comparar por apellido (ascendente). Si coincide el apellido entonces se considera el
+	 * nombre. Este será su criterio natural de ordenación.
+	 * @return 	entero negativo si this es anterior a o.
+	 * 			0 si son equivalentes en orden.
+	 * 			entero positivo si this es posterior a o.
+	 */
+	@Override
+	public int compareTo(Contacto o) {
+		int compApellidos = this.apellidos.compareTo(o.getApellidos());
+		if(compApellidos == 0){
+			return this.nombre.compareTo(o.getNombre());
+		}
+		else{ //se podría quitar el else. Pero lo dejo para que se vea explícitamente el flujo
+			return compApellidos;
+		}
+	}
+
+	/**
+	 * Todos los contactos tienen una firma de email que generan cuando envían un mensaje. El texto concreto de
+	 * esta firma depende del tipo de contacto.
+	 * @return la firma del contacto en String
+	 */
+	public abstract String getFirmaEmail();
+
+	/**
+	 *
+	 * @return la representación textual del Contacto
+	 */
+	@Override
+	public String toString() {
+		return apellidos + ", " + nombre +
+				" (" + getClass().getSimpleName().toUpperCase() + ")" + "\n"
+				+ "Tfno: " + telefono + " | email: " + email;
+	}
 }
--- src/ut7/agenda/modelo/Profesional.java
+++ src/ut7/agenda/modelo/Profesional.java
@@ -1,9 +1,51 @@
 package ut7.agenda.modelo;
 
-public class Profesional {
+import java.util.Random;
 
-	public Profesional() {
+public class Profesional extends Contacto{
+	String empresa;
 
+	/**
+	 * Constructor de la clase Profesional
+	 * El nombre de la empresa se guarda siempre capitalizado,
+	 */
+	public Profesional(String nombre, String apellidos, String telefono, String email, String empresa) {
+		super(nombre, apellidos, telefono, email);
+		this.empresa = capitaliza(empresa); //capitaliza está declarado en la clase Contacto, como método estático
 	}
 
+	public String getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+	@Override
+	public String toString() {
+		return super.toString() + "\n" +
+				"empresa: " + empresa + "\n";
+	}
+
+	/**
+	 * Función añadida para capitalizar el nombre de la empresa
+	 */
+	private String capitaliza(String dato){
+		String[] palabras = dato.split(" ");
+		String retorno="";
+		for (String p : palabras) {
+			if(!p.isEmpty()){
+				retorno += p.toUpperCase().charAt(0) + p.toLowerCase().substring(1);
+				retorno+=" ";
+			}
+		}
+		return retorno.trim();
+	}
+
+	@Override
+	public String getFirmaEmail() {
+		String[] firmasPosibles = {"Atentamente", "Saludos", "Saludos cordiales" , "Mis mejores deseos"};
+		int posAl= new Random().nextInt(firmasPosibles.length);
+		return firmasPosibles[posAl];
+	}
 }
