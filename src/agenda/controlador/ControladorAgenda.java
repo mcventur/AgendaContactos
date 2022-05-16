b55ca186ebb7bebec8359a719a49e67ec544fe70
mcventur <marcelinopenide@gmail.com>
2022-05-16 16:26:35 +0200

    Iniciamos código controlador

--- src/agenda/controlador/ControladorAgenda.java
+++ src/agenda/controlador/ControladorAgenda.java
@@ -1,18 +1,318 @@
 package agenda.controlador;
 
+import agenda.io.AgendaIO;
 import agenda.modelo.AgendaContactos;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ControladorAgenda {
-	private AgendaContactos agenda; // el modelo;
 
-	public ControladorAgenda() {
-		this.agenda = new AgendaContactos();
-	}
+	AgendaContactos agenda;
+	@FXML
+	private TextArea areaTexto;
 
 	@FXML
-	public void initialize() {
+	private MenuBar barraMenu;
+
+	@FXML
+	private Button btnClear;
+
+	@FXML
+	private Button btnListar;
+
+	@FXML
+	private Button btnPersonalesFechaNac;
+
+	@FXML
+	private Button btnPesonalesEnLetra;
+
+	@FXML
+	private Button btnSalir;
+
+	@FXML
+	private MenuItem itemAbout;
+
+	@FXML
+	private MenuItem itemBuscar;
+
+	@FXML
+	private MenuItem itemExportar;
+
+	@FXML
+	private MenuItem itemFelicitar;
+
+	@FXML
+	private MenuItem itemImportar;
+
+	@FXML
+	private MenuItem itemSalir;
+
+	@FXML
+	private GridPane panelLetras;
+
+	@FXML
+	private RadioButton rbtListarNumero;
+
+	@FXML
+	private RadioButton rbtListarTodos;
+
+	@FXML
+	private ToggleGroup toogleLista;
+
+	@FXML
+	private TextField txtBuscar;
+
+	public ControladorAgenda() {
+		agenda = new AgendaContactos();
+	}
+
+	void initialize(){
+		for (Node child : panelLetras.getChildren()) {
+			if(child instanceof Button){
+				((Button) child).setOnAction(e->contactosEnLetra());
+			}
+		}
+	}
+	private void importarAgenda() {
+		FileChooser selector = new FileChooser();
+		selector.setTitle("Abrir fichero csv");
+		selector.setInitialDirectory(new File("."));
+		selector.getExtensionFilters()
+				.addAll(new FileChooser.ExtensionFilter("csv",
+						"*.csv"));
+		File f = selector.showOpenDialog(null);
+		if (f != null) {
+			System.out.println("Fichero elegido: "
+					+ f.getName());
+			try {
+				int errores = AgendaIO.importar(agenda, f.getAbsolutePath());
+				areaTexto.setText("Agenda importada\nLíneas erróneas: " + errores);
+				itemImportar.setDisable(true);
+				itemExportar.setDisable(false);
+			} catch (IOException e) {
+				areaTexto.setText(e.getMessage());
+			}
+		}
 
 	}
 
+	private void exportarPersonales() {
+		FileChooser selector = new FileChooser();
+		selector.setTitle("Exportar contactos personales por relación");
+		selector.setInitialDirectory(new File("."));
+		selector.getExtensionFilters()
+				.addAll(new FileChooser.ExtensionFilter("txt",
+						"*.txt"));
+		File f = selector.showSaveDialog(null);
+		if (f != null) {
+			try {
+				AgendaIO.exportarPersonales(agenda, f.getAbsolutePath());
+				areaTexto.setText("Exportados datos personales");
+			} catch (IOException e) {
+				areaTexto.setText(e.getMessage());
+			}
+		}
+
+	}
+
+	/**
+	 *
+	 */
+	private void listar() {
+		clear();
+		if(itemImportar.isDisable()){//Ya se ha hecho la importacion
+			if (rbtListarTodos.isSelected()) {
+				areaTexto.setText(agenda.toString());
+			} else {
+				areaTexto.setText("Número de contactos: " + agenda.totalContactos()+"");
+			}
+		}
+		else{
+			areaTexto.setText(FALTA_IMPORTAR);
+		}
+
+	}
+
+	private void personalesOrdenadosPorFecha() {
+		clear();
+		if (itemImportar.isDisable()) {
+			Optional<Character> resul = choiceAbecedario();
+			if (resul.isPresent()) {
+				Character opcion = resul.get();
+				List<Personal> contactos = agenda.personalesOrdenadosPorFechaNacimiento(opcion);
+				if(contactos.isEmpty()){
+					areaTexto.setText("No se han encontrado contactos para la letra " + opcion);
+				}
+				else{
+					StringBuilder sb = new StringBuilder("Personales en la letra " + opcion + " ordenados por fecha" +
+							"de nacimiento \n\n");
+					for (Personal c : contactos) {
+						sb.append(c).append("\n");
+					}
+					areaTexto.setText(sb.toString());
+				}
+			}
+			else {
+				//se ha pulsado cancel en el ChoiceDialog
+			}
+		}
+		else {
+			areaTexto.setText(FALTA_IMPORTAR);
+		}
+
+	}
+
+	private void contactosPersonalesEnLetra() {
+		clear();
+		if (itemImportar.isDisable()) {
+			Optional<Character> resul = choiceAbecedario();
+			if (resul.isPresent()) {
+				Character opcion = resul.get();
+				List<Personal> contactos = agenda.personalesEnLetra(opcion);
+
+				try {
+					if(contactos.isEmpty()){
+						areaTexto.setText("No se han encontrado contactos para la letra " + opcion);
+					}
+					else{
+						StringBuilder sb = new StringBuilder("Personales en la letra " + opcion + "\n\n");
+						for (Personal c : contactos) {
+							sb.append(c).append("\n");
+						}
+						areaTexto.setText(sb.toString());
+					}
+				} catch (NullPointerException e) {
+					areaTexto.setText("No se han encontrado contactos para la letra " + opcion);
+				}
+			}
+			else {
+				//se ha pulsado cancel en el ChoiceDialog
+				//No hacemos nada
+			}
+		}
+		else {
+			areaTexto.setText(FALTA_IMPORTAR);
+		}
+
+	}
+
+	private Optional<Character> choiceAbecedario() {
+		String abecedario = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+		Character[] arrLetras = new Character[abecedario.length()];
+		for (int i = 0; i < abecedario.length(); i++) {
+			arrLetras[i]=abecedario.charAt(i);
+		}
+		ChoiceDialog<Character> dialogo = new ChoiceDialog<>('A', Arrays.asList(arrLetras));
+		dialogo.setTitle("Selector de letra");
+		dialogo.setHeaderText(null);
+		dialogo.setContentText("Elija letra");
+		Optional<Character> resul = dialogo.showAndWait();
+		return resul;
+	}
+
+	private void contactosEnLetra(char letra) {
+		clear();
+		if(itemImportar.isDisable()){//Ya se ha hecho la importacion
+			Set<Contacto> contactos = agenda.contactosEnLetra(letra);
+			if(contactos==null || contactos.isEmpty()){
+				areaTexto.setText("No hay contactos en la letra " + letra);
+			}
+			else{
+				StringBuilder sb = new StringBuilder("Contactos en la letra " + letra + "\n\n");
+				for (Contacto c : contactos) {
+					sb.append(c).append("\n");
+				}
+				areaTexto.setText(sb.toString());
+			}
+
+		}
+		else{
+			areaTexto.setText(FALTA_IMPORTAR);
+		}
+	}
+
+	private void felicitar() {
+		clear();
+		if(itemImportar.isDisable()){//Si ya se han importado los contactos
+			StringBuilder sb = new StringBuilder("Hoy es " + fechaHoy() + "\n\n");
+			List<Personal> felicidades = agenda.felicitar();
+			if(felicidades.isEmpty()){
+				sb.append("No hay contactos para felicitar");
+			}
+			else{
+				sb.append("Hoy hay que felicitar a\n\n");
+				for (Personal p : felicidades) {
+					sb.append(p).append("\n");
+				}
+			}
+			areaTexto.setText(sb.toString());
+		}
+		else{
+			areaTexto.setText("Debe importar los contactos antes de poder felicitar");
+		}
+	}
+
+	private String fechaHoy() {
+		LocalDate hoy = LocalDate.now();
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/YYYY");
+		return hoy.format(dtf).toString();
+	}
+
+	private void buscar() {
+		clear();
+		String buscar = txtBuscar.getText();
+		if (buscar.isEmpty()) {
+			areaTexto.setText("Introduzca el texto a buscar");
+		} else {
+			StringBuilder texto = new StringBuilder("Contactos en la agenda que contienen '" + buscar + "'\n\n");
+			List<Contacto> encontrados = agenda.buscarContactos(buscar);
+			if (encontrados.isEmpty()) {
+				texto.append("No se han encontrado contactos");
+			} else {
+				for (Contacto contacto : encontrados) {
+					texto.append(contacto).append("\n");
+				}
+			}
+			areaTexto.setText(texto.toString());
+		}
+		cogerFoco();
+
+	}
+
+	private void about() {
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setTitle("About Agenda de contactos");
+		alert.setHeaderText(null);
+		DialogPane dialogPane = alert.getDialogPane();
+		dialogPane.getStylesheets().add(getClass().
+				getResource("/application.css").toExternalForm());
+		alert.setContentText("Mi agenda de contactos");
+		alert.showAndWait();
+	}
+
+	private void clear() {
+		areaTexto.setText("");
+	}
+
+	private void salir() {
+		Platform.exit();
+	}
+
+	private void cogerFoco() {
+		txtBuscar.requestFocus();
+		txtBuscar.selectAll();
+
+	}
 }
